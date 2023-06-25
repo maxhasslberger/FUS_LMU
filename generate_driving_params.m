0fea08cfbc2392a26e1ebb2c427be6850e1c12e2
Max Hasslberger <max.hasslberger@tum.de>
2023-06-25 21:56:01 +0200

    Bug fix + files for min/max focus depths

--- generate_driving_params.m
+++ generate_driving_params.m
@@ -1,6 +1,6 @@
 % Generate all the files:
-% generate_driving_params(13 + (34:62), 'CTX500');
-% generate_driving_params(13 + [34, 62], 'CTX500');
+% generate_driving_params(13 + (34:62), 'CTX500', 0);
+% generate_driving_params(13 + [34, 62], 'CTX500', 0);
 
 function [pressure, phase] = generate_driving_params(focus_depths, transducer, isppa_device, amp)
 
@@ -24,6 +24,7 @@ for focus_depth = focus_depths % 13+34 %47:82
 %         phase = phase_correction(focus_depth, transducer); % [Pa, deg]
     
         for lut_idx = 1:length(amp)
+            disp(num2str(lut_idx) + "/" + num2str(length(amp)))
 %             get_source_pressure(transducer, phase, focus_depth, 25);
             Isppa = tussim_water_3D(transducer, amp(lut_idx), phase);
     
@@ -41,6 +42,7 @@ for focus_depth = focus_depths % 13+34 %47:82
 
     [~,idx]=min(abs(lut.isppa-isppa_device));
     pressure = lut.pressure(idx);
+    clear lut;
 end
 
 end
