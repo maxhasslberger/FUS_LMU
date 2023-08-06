1525f662a4e892e69e202a987617e3f289df804a
Max Hasslberger <max.hasslberger@tum.de>
2023-08-06 14:06:02 +0200

    Load sham axis and pressure from subject_real file

--- run_sequence.m
+++ run_sequence.m
@@ -6,8 +6,8 @@ clear;
 % subj.id = 'FUNtest01';
 % subj.id = 'FUN0000';
 % subj.id = 'FUN0001';
-subj.id = 'FUN0002';
-% subj.id = 'FUN0003';
+% subj.id = 'FUN0002';
+subj.id = 'FUN0003';
 % subj.id = 'FUN0004';
 % subj.id = 'FUN0005';
 % subj.id = 'FUN0006';
@@ -21,7 +21,7 @@ subj.id = 'FUN0002';
 % subj.id = 'FUN0014';
 
 explorative_sim = true;
-% condition = "real";
+% condition = "real"; % If first simulation: -> First real, then sham!
 condition = "sham";
 
 % Simulation options
@@ -41,21 +41,17 @@ subj_filename = fullfile('subject_init_params/', strcat(subj.id, "_", condition,
 if ~exist(subj_filename, 'file') || explorative_sim
 %% Modify params
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    subj.t1_filename = fullfile(filepath, 'FUN0002t1_T1w_MPRAGE_t1_20230520110230_9.nii');
-    subj.ct_filename = fullfile(filepath, 'FUN0002t1_T1w_MPRAGE_t1_20230520110230_9_pct.nii');
+    subj.t1_filename = fullfile(filepath, 'FUN0003t1_T1w_MPRAGE_t1_20230520133911_9.nii');
+    subj.ct_filename = fullfile(filepath, 'FUN0003t1_T1w_MPRAGE_t1_20230520133911_9_pct.nii');
 
-    subj.offset = [96, 112, 155];
-
-    if sham_cond
-        subj.bowl_coord_axis_origin = [-68, -24, 78];
-        subj.pressure = 42272;
-    end
+    subj.offset = [96, 127, 126];
 
     % Explorative params
-    expl.focus_coords_mm_orig = [-8, -19, -1]; % Desired FT or sham focus position - adjust for large diffractions introduced by the skull
-    expl.min_pad_offset = 12; % min. pad offset (added to hair offset (~3 mm))
-    expl.add_offset = 6; % additional pad offset after focus_depth computation
-    expl.bowl_coord_axis = [124, 126, 134]; % Adjust relative bowl position (focus at (128, 128, 128))
+    expl.focus_coords_mm_orig = [-13, -10, 26]; % Desired FT or sham focus position - adjust for large diffractions introduced by the skull
+    expl.min_pad_offset = 10; % min. pad offset (added to hair offset (~3 mm))
+    expl.add_offset = 0; % additional pad offset after focus_depth computation
+
+    expl.bowl_coord_axis = [124, 127, 133]; % Adjust relative bowl position (focus at (128, 128, 128))
     expl.isppa_device = 20; % W/cm^2 - FDA ISPTA limit = 720 mW/cm^2
 
 
@@ -99,7 +95,12 @@ if ~exist(subj_filename, 'file') || explorative_sim
 %     close all;
 
     if sham_cond
-        subj.expl.bowl_coord_axis = subj.bowl_coord_axis_origin + subj.offset;
+        subj_filename_real = fullfile('subject_init_params/', strcat(subj.id, '_real.mat'));
+        subj_real = load(subj_filename_real, 'subj');
+
+        subj.expl.bowl_coord_axis = subj_real.subj.bowl_coords_mm;
+        subj.pressure = subj_real.subj.pressure;
+        subj.expl.bowl_coord_axis = subj.expl.bowl_coord_axis + subj.offset;
         subj.expl.bowl_coord_axis = subj.expl.bowl_coord_axis - (subj.focus_coords - focus_coords_rel);
     end
 
@@ -117,7 +118,9 @@ if ~exist(subj_filename, 'file') || explorative_sim
         [~, subj.phase] = get_driving_params(subj.focus_depth_mm, subj.transducer); % [Pa, deg]
     %     [~, subj.phase] = generate_driving_params(focus_depth_mm, transducer, isppa_device); % [Pa, deg]
     end
-    
+
+    subj.bowl_coords_mm = ((subj.bowl_coords / 1e-3 .* subj.dxyz) - subj.offset); % mm
+    subj.focus_depth_mm_NeuroFUS = subj.focus_depth_mm - 13; % Distance to device face
 
     save(subj_filename, 'subj');
 else
@@ -125,11 +128,8 @@ else
 end
 
 % Display important params
-bowl_coords_mm = ((subj.bowl_coords / 1e-3 .* subj.dxyz) - subj.offset); % mm
-focus_depth_mm_NeuroFUS = subj.focus_depth_mm - 13; % Distance to device face
-
-disp("Bowl_coords_mm: " + num2str(bowl_coords_mm))
-disp("focus_depth_mm_NeuroFUS: " + num2str(focus_depth_mm_NeuroFUS))
+disp("Bowl_coords_mm: " + num2str(subj.bowl_coords_mm))
+disp("focus_depth_mm_NeuroFUS: " + num2str(subj.focus_depth_mm_NeuroFUS))
 disp("k-wave Pressure: " + num2str(subj.pressure))
 disp("transducer_angle: " + num2str(subj.transducer_angle))
 disp("pad_offset_mm: " + num2str(subj.pad_offset_mm))
